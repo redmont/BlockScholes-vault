e08a73d81f1f0f2e74da83c6b0b5386771de8b77
redmont <dnshu.skynet@gmail.com>
2024-06-27 02:38:02 +0530

    added immutable and other gas refactor

--- Vault.sol
+++ Vault.sol
@@ -1,79 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IWETH{
+interface IWETH {
     function deposit() external payable;
     function withdraw(uint256) external;
 }
 
-//  //0x0000000000000000000000000000000000000000 - ETH address
-    //0xfff9976782d46cc05630d1f6ebab18b2324d6b14 - WETH sepolia
-    
-
-contract Vault{
-
+contract Vault {
     event Deposit(address user, address token, uint256 amount);
     event Withdraw(address user, address token, uint256 amount);
     event ETHWrapped(address user, uint256 amount);
     event ETHUnwrapped(address user, uint256 amount);
-    
-    mapping(address=> mapping(address =>uint256)) public balances;
-    address public wETHAddr;
-    IWETH wETH;
+
+    mapping(address => mapping(address => uint256)) public balances;
+    IWETH public immutable wETH;
 
     constructor(address _wETHAddr) {
         wETH = IWETH(_wETHAddr);
-        wETHAddr = _wETHAddr;
     }
 
-    function depositETH(uint256 value) external payable {
-        deposit(address(0),value);
+    function depositETH() external payable {
+        require(msg.value > 0, "Value must be greater than zero");
+        balances[msg.sender][address(0)] += msg.value;
+        emit Deposit(msg.sender, address(0), msg.value);
     }
-    function deposit(address _token, uint256 _value) public payable {
-        require(_value>0, "value should be greater than zero");
 
-        if(_token == address(0)){
-            balances[msg.sender][address(0)] += msg.value;
-        }
-        else {
-            IERC20(_token).transferFrom(msg.sender, address(this), _value);
-            balances[msg.sender][_token] += _value;
-        }
+    function deposit(address _token, uint256 _value) external {
+        require(_value > 0, "Value must be greater than zero");
+        IERC20(_token).transferFrom(msg.sender, address(this), _value);
+        balances[msg.sender][_token] += _value;
         emit Deposit(msg.sender, _token, _value);
     }
 
-    function withdrawETH(uint256 value) external {
-        withdraw(address(0),value);
+    function withdrawETH(uint256 _value) external {
+        require(balances[msg.sender][address(0)] >= _value, "Insufficient balance");
+        balances[msg.sender][address(0)] -= _value;
+        (bool success, ) = msg.sender.call{value: _value}("");
+        require(success, "Transfer failed");
+        emit Withdraw(msg.sender, address(0), _value);
     }
 
-    function withdraw(address _token, uint256 _value) public{
+    function withdraw(address _token, uint256 _value) external {
         require(balances[msg.sender][_token] >= _value, "Insufficient balance");
-        if(_token == address(0)){
-            (bool success, ) = msg.sender.call{value:_value}("");
-            require(success, "Transfer ETH Failed");
-            balances[msg.sender][_token] -= _value;
-        }
-        else {
-            IERC20(_token).transfer(msg.sender, _value);
-            balances[msg.sender][_token] -= _value;
-        }
+        balances[msg.sender][_token] -= _value;
+        IERC20(_token).transfer(msg.sender, _value);
         emit Withdraw(msg.sender, _token, _value);
     }
-    function wrapETH(uint256 amount) external{
-        require(balances[msg.sender][address(0)] >= amount, "Insuffient ETH");
-        wETH.deposit{value:amount}();
-        balances[msg.sender][address(0)] -= amount;
-        balances[msg.sender][wETHAddr] += amount;
-        emit ETHWrapped(msg.sender,amount);
+
+    function wrapETH(uint256 _amount) external {
+        require(balances[msg.sender][address(0)] >= _amount, "Insufficient ETH");
+        balances[msg.sender][address(0)] -= _amount;
+        balances[msg.sender][address(wETH)] += _amount;
+        wETH.deposit{value: _amount}();
+        emit ETHWrapped(msg.sender, _amount);
     }
 
-    function unwrapETH(uint256 amount) external{
-        require(balances[msg.sender][wETHAddr] >= amount, "Insuffient wETH");
-        wETH.withdraw(amount);
-        balances[msg.sender][address(0)] += amount;
-        balances[msg.sender][wETHAddr] -= amount;
-        emit ETHUnwrapped(msg.sender,amount);
+    function unwrapETH(uint256 _amount) external {
+        require(balances[msg.sender][address(wETH)] >= _amount, "Insufficient wETH");
+        balances[msg.sender][address(wETH)] -= _amount;
+        balances[msg.sender][address(0)] += _amount;
+        wETH.withdraw(_amount);
+        emit ETHUnwrapped(msg.sender, _amount);
     }
 }
